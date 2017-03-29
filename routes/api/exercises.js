a53668f1aeaef9ef439ef5d3d267b268196dc257
Nichole De La Cruz <nichole.dlc@gmail.com>
2017-03-29 16:29:39 -0700

    add Exercises#show for api

--- routes/api/exercises.js
+++ routes/api/exercises.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {Exercise} = require('../../models/index');
 
-// URL: /api/exercises, METHOD: GET
+// Exercises#index, URL: /api/exercises, METHOD: GET
 router.get('/', function(req, res, next) {
   Exercise
     .findAll()
@@ -11,5 +11,18 @@ router.get('/', function(req, res, next) {
     })
     .catch(res.send);
 });
+
+// Exercise#show, URL: /api/exercises/:id, METHOD: GET
+router.get('/:id', function(req, res, next) {
+  const {id} = req.params;
+  Exercise
+    .findById(id)
+    .then(exercise => {
+      res.json({exercise})
+    })
+    .catch(error => {
+      res.json({error: {name: err.name, message: err.message}})
+    })
+})
 
 module.exports = router;
