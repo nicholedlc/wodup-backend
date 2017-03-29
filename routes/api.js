3b698ac003d5202a25428a741ac777ac517970a3
Nichole De La Cruz <nichole.dlc@gmail.com>
2017-03-29 16:34:46 -0700

    add support for all HTTP verbs in api routes

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const exercises = require('./api/exercises');
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  // The header Access-Control-Allow-Headers is used in a CORS pre-flight to tell the client which HTTP headers are allowed in the request
+  res.header("Access-Control-Allow-Headers", //this is the header
+            "Origin, X-Requested-With, Content-Type, Accept"); //this is the value
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  next();
+});
+
 router.use('/exercises', exercises);
 
 module.exports = router;
