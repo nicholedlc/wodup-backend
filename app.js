f43d3203ecf5610a0b4592d140f5e2ccc687b15e
Nichole De La Cruz <nichole.dlc@gmail.com>
2017-03-30 11:10:07 -0700

    change PORT to 3636

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', index);
 app.use('/api', api);
 app.use('/users', users);
 
