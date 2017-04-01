db08305d76dc5c8d11f671b4555ccee78f090f18
Nichole De La Cruz <nichole.dlc@gmail.com>
2017-04-01 11:40:00 -0700

    Draft user authentication with JWT

--- app.js
+++ app.js
@@ -4,12 +4,45 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const {ExtractJwt, Strategy} = passportJWT;
 
 const api = require('./routes/api')
 const index = require('./routes/index');
-const users = require('./routes/users');
+// const users = require('./routes/users');
 
 const app = express();
+
+const users = [
+  {
+    id: 1,
+    name: 'jonathanmh',
+    password: '%2yx4'
+  },
+  {
+    id: 2,
+    name: 'test',
+    password: 'test'
+  }
+];
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeader(),
+  // The secretOrKey is the secret that our tokens will be signed with. Choose this wisely or use a private key.
+  secretOrKey: 'iLoveSteve'
+}
+
+// jwt_payload is the data decrypted from the jwt token, which is part of the request (it's going to be in the header)
+// this strategy is used to find the user
+const strategy = new Strategy(jwtOptions, ((jwt_payload, next) => {
+  console.log('payload received', jwt_payload);
+  const user = users.find(({id}) => jwt_payload.id === id);
+  user ? next(null, user) : next(null, false);
+}));
+passport.use(strategy);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +51,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(passport.initialize());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -25,7 +59,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/api', api);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const exercises = require('./api/exercises');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const {ExtractJwt, Strategy} = passportJWT;
 
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -9,6 +13,70 @@ router.use(function(req, res, next) {
             "Origin, X-Requested-With, Content-Type, Accept"); //this is the value
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   next();
+});
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeader(),
+  // The secretOrKey is the secret that our tokens will be signed with. Choose this wisely or use a private key.
+  secretOrKey: 'iLoveSteve'
+}
+
+const users = [
+  {
+    id: 1,
+    name: 'jonathanmh',
+    password: '%2yx4'
+  },
+  {
+    id: 2,
+    name: 'test',
+    password: 'test'
+  }
+];
+
+// fetch(`http://localhost:3636/api/login`, {
+//     headers: {
+//           'Accept': 'application/json, text/plain, */*',
+//           'Content-Type': 'application/json'},
+//     method: 'POST',
+//     body: JSON.stringify({
+//         username: 'jonathanmh',
+//         password: '%2yx4'
+//     })
+// })
+// .then(r => r.json())
+
+router.post('/login', function(req, res, next) {
+  const {username, password} = req.body;
+  if (username && password) {
+    const user = users.find(({name}) => {
+      return username === name;
+    })
+    if (!user) {
+      res.status(401).json({message:"no such user found"});
+    }
+    else if (user.password === password) {
+      const payload = {id: user.id};
+      const token = jwt.sign(payload, jwtOptions.secretOrKey);
+      res.json({message: 'ok', token: token});
+    } else {
+      res.status(401).json({message: 'passwords did not match'});
+    }
+  }
+});
+
+// fetch(`http://localhost:3636/api/supersecret`, {
+//     headers: {
+//           'Accept': 'application/json, text/plain, */*',
+//           'Content-Type': 'application/json',
+//           'Authorization': "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNDkxMDY4NzI2fQ.oLkuUm1bv3XJeSbxxPnsO6Rjhg_Tr3br3XzNxwTxPI4"},
+//     method: 'GET'
+// })
+// .then(r => r.json())
+// .then(console.info);
+
+router.get('/supersecret', passport.authenticate('jwt', {session: false}), function(req, res, next) {
+  res.json(req.user);
 });
 
 router.use('/exercises', exercises);
